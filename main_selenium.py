b9b9d23243980debcad2f1442c05bc8cd528a6cf
dbouquin <dbouquin@gmail.com>
2023-08-03 15:18:15 -0400

    remove unused imports

--- main_selenium.py
+++ main_selenium.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import json
 import zipfile
