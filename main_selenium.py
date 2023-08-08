9ca3d3c405d0c73c88a043d6d84f22ddeae6c72f
dbouquin <dbouquin@gmail.com>
2023-08-08 11:42:01 -0400

    map dirs with .json file

--- main_selenium.py
+++ main_selenium.py
@@ -7,64 +7,79 @@ import json
 import zipfile
 import os
 import time
+import shutil
 
-
+# Function to get the latest file in a directory
 def get_latest_file_in_directory(directory):
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
     return files[-1] if files else None
 
-
+# Function to unzip a file
 def unzip_file(zip_filepath, dest_dir):
     with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
         zip_ref.extractall(dest_dir)
 
-
+# Main function
 def main():
     try:
+        # Load the URL from the file
         with open('link.txt', 'r') as f:
             url = f.read().strip()
 
+        # Load the credentials from the file
         with open('credentials.json', 'r') as f:
             credentials = json.load(f)
 
+        # Load the job number from the file
         with open('job.txt', 'r') as f:
             job_number = f.read().strip()
 
+        # Load the directory map from the JSON file
+        with open('map.json', 'r') as f:
+            directory_map = json.load(f)
+
+        # Set up the Chrome options
         chrome_options = Options()
         chrome_options.add_argument("--headless")  # GUI off
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        prefs = {"download.default_directory": "/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files"}
+        # Set the default download directory
+        prefs = {"download.default_directory": "/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad"}
         chrome_options.add_experimental_option("prefs", prefs)
 
+        # Set up the WebDriver service
         webdriver_service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
+        # Navigate to the URL
         driver.get(url)
 
+        # Fill in the form
         driver.find_element(By.ID, 'loginuserid').send_keys(credentials['username'])
         driver.find_element(By.ID, 'loginpassword').send_keys(credentials['password'])
         driver.find_element(By.ID, 'loginclient').send_keys(credentials['client_code'])
 
+        # Submit the form
         driver.find_element(By.CSS_SELECTOR, '#frmlogin2 button[type="submit"].btn.btn-default').click()
 
+        # Wait for the download to start
         time.sleep(5)
 
-        downloaded_file = get_latest_file_in_directory(
-            '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files'
-        )
+        # Get the name of the latest file in the download directory
+        downloaded_file = get_latest_file_in_directory('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad')
 
+        # If the downloaded file is a ZIP file and contains the job number, unzip it
         if downloaded_file and downloaded_file.endswith('.ZIP') and job_number in downloaded_file:
             unzip_file(
-                os.path.join(
-                    '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files', downloaded_file
-                ),
-                '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files'
+                os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', downloaded_file),
+                '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad'
             )
 
-            for file in os.listdir('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files'):
+            # Loop through all the files in the download directory
+            for file in os.listdir('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad'):
+                # If the file is a CSV file and starts with 'NPCA', rename it
                 if file.startswith('NPCA') and file.endswith('.csv'):
                     # Split the filename into the prefix and the rest
                     prefix, rest_of_filename = file.split('NPCA_JOB', 1)
@@ -74,38 +89,38 @@ def main():
                     if job_number_in_file == job_number:
                         new_filename = rest_of_filename
                         os.rename(
-                            os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files', file),
-                            os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files', new_filename)
+                            os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', file),
+                            os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', new_filename)
                         )
 
+                        # Move the file to the correct directory based on its name
+                        for keyword, directory in directory_map.items():
+                            if keyword in new_filename.upper():
+                                shutil.move(
+                                    os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', new_filename),
+                                    os.path.join(directory, new_filename)
+                                )
+                                break
+
+            # Remove the original ZIP file
             os.remove(
                 os.path.join(
-                    '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files', downloaded_file
+                    '/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', downloaded_file
                 )
             )
 
             # Remove all .txt files
-            for file in os.listdir('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files'):
+            for file in os.listdir('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad'):
                 if file.endswith('.TXT'):
-                    os.remove(os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/rescued_roi_files', file))
+                    os.remove(os.path.join('/Users/dbouquin/OneDrive/Documents_Daina/hackbot/landing_pad', file))
 
+        # Close the browser
         driver.quit()
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
+# Run the main function
 main()
 
-
-#TODO: look into using json dict to store configs for reports and dir locations
-
 #%%
-
-# Idea:
-# + All ROI reports run 15 mins apart, emails received soon after
-# + Power Automate runs after each email is received; stores link.txt and job.txt in OneDrive
-# + Selenium script runs 10 minutes after each report is scheduled to run
-# + After Selenium script runs on last report, a new python script checks configs
-#   for the report name and stores the file in the correct directory
-# Consisider link and job file naming.
-# Maybe add report name to file name and create separate selenium scripts for each report
